816c6e4405b101e7f30055417e6e82333a5ddcaf
Jon Sanchez <jonathan.sanchez@windstream.net>
2020-12-20 05:08:49 -0700

    Add all Responses

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -47,23 +48,21 @@ const db = new sqlite3.Database('./db/election.db', err => {
 // });
 
 // Get all candidates
-// Get single candidate
-app.get('/api/candidate/:id', (req, res) => {
-  const sql = `SELECT * FROM candidates 
-               WHERE id = ?`;
-  const params = [req.params.id];
-  db.get(sql, params, (err, row) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-
-    res.json({
-      message: 'success',
-      data: row
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
     });
   });
-});
 
 // Get single candidate
 app.get('/api/candidate/:id', (req, res) => {
@@ -100,6 +99,31 @@ app.delete('/api/candidate/:id', (req, res) => {
     });
   });
 
+  // Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+    VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use `this`
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+    }
+
+    res.json({
+        message: 'success',
+        data: body,
+        id: this.lastID
+        });
+    });
+
+});
 
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
