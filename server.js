a348699115edd2dc35f9632124366420793c514f
Jon Sanchez <jonathan.sanchez@windstream.net>
2020-12-20 04:48:20 -0700

    GET DELETE

--- server.js
+++ server.js
@@ -47,18 +47,55 @@ const db = new sqlite3.Database('./db/election.db', err => {
 // });
 
 // Get all candidates
-app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
-    const params = [];
-    db.all(sql, params, (err, rows) => {
+// Get single candidate
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates 
+               WHERE id = ?`;
+  const params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+// Get single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates 
+                 WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
       if (err) {
-        res.status(500).json({ error: err.message });
+        res.status(400).json({ error: err.message });
         return;
       }
   
       res.json({
         message: 'success',
-        data: rows
+        data: row
+      });
+    });
+  });
+
+  // Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      }
+  
+      res.json({
+        message: 'successfully deleted',
+        changes: this.changes
       });
     });
   });
